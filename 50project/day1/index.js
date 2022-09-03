9269973ae417ef32fb47027d5d7db453a2e33c0b
ruansheng <1156341958@qq.com>
2022-09-03 15:12:11 +0800

    fix 1day样式js问题

--- 50project/day1/index.js
+++ 50project/day1/index.js
@@ -14,7 +14,12 @@ for (let i = 0; i < 4; ++i) {
         const box = document.createElement('div');
         //添加样式
         box.classList.add('box');
-        //
+        //每个元素的样式 宽高都为125px 也就是一个125的小窗 每个box的背景都是同一张图  根据这个来设置位置显示不同的地方
+        //第一个块 显示的是 -125px 0px   xy坐标  浏览器的xy左边是默认在左上角为0.0点
+        /*
+         * 第一行 第一个坐标 0 0 然后加上盒子的宽高 125 125 显示的图内容就是 125,125
+         * 反正这个计算还是挺精巧的 反正就是用125宽高的视窗盒子来显示图片的一小块一小块
+         */
         box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
         //将盒子添加到外层
         boxs.appendChild(box);
