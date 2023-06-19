1baaab6c543e8249110b101c4c071cba83fd8854
ruansheng <1156341958@qq.com>
2023-06-19 15:28:57 +0800

    add

--- 50project/day7/1.js
+++ 50project/day7/1.js
@@ -16,7 +16,7 @@ function blurring() {
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
     //高斯模糊 从100到0
     // bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-    bg.style.filter = scale(load, 0, 100, 30, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
 //看不太懂的公司 但是根据链接看到的信息是 给定一个范围 将输入值映射到这段范围内的一个值  数学不好看不懂 哎
