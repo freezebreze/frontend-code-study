376626f928c72c07d6a4970d12787cdd6a85b7a3
ruansheng <1156341958@qq.com>
2023-08-04 16:36:23 +0800

    add quickSortfun

--- function/sort.js
+++ function/sort.js
@@ -1,5 +1,5 @@
 let array = [4, 5, 8, 9, 1, 0, 3, 2, 7, 6]
-
+let quickarr = [2, 3, 1, 4, 8, 7, 9, 6]
 
 /**
  * 快速排序
@@ -7,9 +7,30 @@ let array = [4, 5, 8, 9, 1, 0, 3, 2, 7, 6]
  * @param {*} begin 
  * @param {*} end 
  */
-function quickSort(array, begin, end) {
-
+function quickSort(arr, begin, end) {
+    if (begin < end) {
+        let i = begin;
+        let j = end;
+        let empty = arr[begin];
+        while (i < j) {
+            while (arr[j] > empty && i < j) {
+                j--;
+            }
+            arr[i] = arr[j];
+            while (arr[i] < empty && i < j) {
+                i++;
+            }
+            arr[j] = arr[i];
+        }
+        arr[i] = empty;
+        sort(arr, begin, i - 1);
+        sort(arr, i + 1, end);
+    } else {
+        return;
+    }
 }
+
+
 
 
 /**
@@ -32,13 +53,13 @@ function bubSort(array) {
     return array
 }
 
-console.log(`冒泡排序: ${bubSort(array)}`)
+// console.log(`冒泡排序: ${bubSort(array)}`)
 
 //立即执行函数的使用
-for (var i = 0; i < 10; i++) {
-    (function (j) {
-        setTimeout(() => {
-            console.log(j)
-        }, 10);
-    })(i)
-}
+// for (var i = 0; i < 10; i++) {
+//     (function (j) {
+//         setTimeout(() => {
+//             console.log(j)
+//         }, 10);
+//     })(i)
+// }
