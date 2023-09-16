4e8335cf273cf0b3f7c391efe4794d76daa31057
ruansheng <1156341958@qq.com>
2023-09-16 23:11:18 +0800

    fix

--- js/prototype.js
+++ js/prototype.js
@@ -6,7 +6,7 @@ function myNew(fn, ...arg) {
     //实例和构造函数是没有直接关系的,需要通过原型链来建立关系  实例->构造函数的原型->构造函数 这样实例和构造函数都继承于一个实例 
     obj.__proto__ = fn.prototype
     //绑定this的值 也就是函数运行 或者实例运行时的上下文环境(变量的环境)
-    let result = fn.apply(obj, age)
+    let result = fn.apply(obj, arg)
     return result instanceof Object? result : obj;
 }
 // 构造函数要是没有显示的return 一个对象 那么就会方法刚才创建的新对象
