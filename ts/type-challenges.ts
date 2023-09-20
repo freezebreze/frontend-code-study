c73c01ec875a3d948f835429944ae82485f9280d
ruansheng <1156341958@qq.com>
2023-09-20 23:41:53 +0800

    omit

--- ts/type-challenges.ts
+++ ts/type-challenges.ts
@@ -2,3 +2,13 @@
 type myReturn<T> = T extends (...arg: any[]) => infer r ? r : never;
 let somePerson: myReturn<() => { name: string; age: number }>;
 let valueofreturn: myReturn<string>;
+//实现内置omit
+type myOmit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+  createdAt: number;
+}
+
+type TodoPreview = myOmit<Todo, "description">;
