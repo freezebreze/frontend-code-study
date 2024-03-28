19baf4b1e0ea7a99b3c2697d48143510807be0d3
ruansheng <1156341958@qq.com>
2024-03-28 18:36:58 +0800

    study

--- function/link.js
+++ function/link.js
@@ -3,9 +3,9 @@ function defaultEquals(a, b) {
 }
 
 class Node {
-    constructor(element) {
+    constructor(element, next) {
         this.element = element;
-        this.next = undefined;
+        this.next = next;
     }
 }
 
@@ -118,4 +118,67 @@ class LinkedList {
         return objString; // {5} 
     }
 
+}
+
+class DoublyNode extends Node {
+    constructor(element, next, prev) {
+        super(element, next);
+        this.prev = prev;
+    }
+}
+
+class DoublyLinkedList extends LinkedList {
+    constructor(equalsFn = defaultEquals) {
+        super(equalsFn);
+        this.tail = undefined;
+    }
+
+    insert(element, index) {
+        if (index >= 0 && index < this.count) {
+            const node = new DoublyNode(element);
+            let current = this.head;
+            //插入头
+            if (index === 0) {
+                if (this.head == null) {
+                    this.head = node
+                    this.tail = node
+                } else {
+                    node.next = this.head;
+                    current.prev = node;
+                    this.head = node;
+                }
+                //插入尾
+            } else if (index == this.count) {
+                current = this.tail;
+                current.next = node;
+                node.prev = current;
+                this.tail = node;
+                //插入中间
+            } else {
+                const previous = this.getElementAt(index - 1);
+                current = previous.next;
+                node.next = current;
+                previous.next = node
+                current.prev = node;
+                node.prev = previous;
+            }
+            this.count++;
+            return true
+        }
+        return false
+    }
+
+    removeAt(index) {
+        if (index >= 0 && index < this.count) {
+            let current = this.head
+            if (index == 0) {
+                this.head = current.next
+                if (this.count === 1) { // {2} 
+                    this.tail = undefined;
+                } else {
+                    this.head.prev = undefined; // {3} 
+                }
+            }
+        }
+    }
 }
