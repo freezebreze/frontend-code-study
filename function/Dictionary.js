ab85d8dbc49cc76a52685742c04f9a8e6c939daa
ruansheng <1156341958@qq.com>
2024-06-03 12:34:06 +0800

    study

--- function/Dictionary.js
+++ function/Dictionary.js
@@ -116,4 +116,65 @@ function defaultToString(item) {
         return `${item}`;
     }
     return item.toString(); // {1} 
-} 
+}
+
+class Graph {
+    constructor(isDirected = false) {
+        this.isDirected = isDirected; // {1} 是否有向
+        this.vertices = []; // {2} 顶点
+        this.adjList = new Dictionary(); // {3}  顶尖作为键的 字典  实现邻接表   v: [a,b,c...]
+    }
+    //添加顶点
+    addVertex(v) {
+        if (!this.vertices.includes(v)) {
+            this.vertices.push(v)
+            this.adjList.set(v, [])
+        }
+    }
+    //添加线
+    addEdge(v, w) {
+        //这两步确保顶点一定存在 而且也顺便初始化邻接表
+        if (!this.adjList.get(v)) {
+            this.addVertex(v); // {8} 
+        }
+        if (!this.adjList.get(w)) {
+            this.addVertex(w); // {9} 
+        }
+        this.adjList.get(v).push(w) //   v:[w]
+        if (this.isDirected) {
+            this.adjList.get(w).push(v) // 有方向的 w：[v] 也要存进来
+        }
+    }
+
+    toString() {
+        let s = '';
+        for (let i = 0; i < this.vertices.length; i++) { // {15} 
+            s += `${this.vertices[i]} -> `;
+            const neighbors = this.adjList.get(this.vertices[i]); // {16} 
+            for (let j = 0; j < neighbors.length; j++) { // {17} 
+                s += `${neighbors[j]} `;
+            }
+            s += '\n'; // {18} 
+        }
+        return s;
+    }
+}
+
+
+const graph = new Graph();
+const myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']; // {12} 
+for (let i = 0; i < myVertices.length; i++) { // {13} 
+    graph.addVertex(myVertices[i]);
+}
+
+graph.addEdge('A', 'B'); // {14}
+graph.addEdge('A', 'C');
+graph.addEdge('A', 'D');
+graph.addEdge('C', 'D');
+graph.addEdge('C', 'G');
+graph.addEdge('D', 'G');
+graph.addEdge('D', 'H');
+graph.addEdge('B', 'E');
+graph.addEdge('B', 'F');
+graph.addEdge('E', 'I');
+console.log(graph.toString()); 
--- js/prototype.js
+++ js/prototype.js
@@ -1,23 +1,99 @@
-//手写一个new
-function myNew(fn, ...arg) {
-    //创建一个新`的对象
-    let obj = {}
-    //指向构造函数的原型   实例上有个[[prototype]] 或者现在浏览器实现的都叫__proto__ 指向构造函数的原型  然后这个原型的constructor指向构造函数
-    //实例和构造函数是没有直接关系的,需要通过原型链来建立关系  实例->构造函数的原型->构造函数 这样实例和构造函数都继承于一个实例 
-    obj.__proto__ = fn.prototype
-    //绑定this的值 也就是函数运行 或者实例运行时的上下文环境(变量的环境)
-    let result = fn.apply(obj, arg)
-    return result instanceof Object? result : obj;
+function Person(name) {
+    this.name = name
 }
-// 构造函数要是没有显示的return 一个对象 那么就会方法刚才创建的新对象
 
-Function.prototype.myBind = function(context) {
-    if (typeof this !== 'function') {
-        throw new TypeError('Error')
-    }
-    const args = [...arguments].slice(1)
-    fn = this
-    return function Fn() {
-        return fn.apply(this instanceof Fn? new fn(...arguments) : context, args.concat(...arguments))
+Person.prototype.getName = function () {
+    return this.name
+}
+
+var objectFactory = function () {
+    var obj = new Object()
+    Constructor = [].shift.call(arguments)
+    obj.__proto__ = Constructor.prototype
+    var ret = Constructor.apply(obj, arguments)
+    return typeof ret === 'object' ? ret : obj;
+}
+var a = objectFactory(Person, 'sven');
+
+Function.prototype.before = function (beforefn) {
+    let __self = this
+    return function () {    // 返回包含了原函数和新函数的"代理"函数 
+        beforefn.apply(this, arguments);     // 执行新函数，修正this 
+        return __self.apply(this, arguments);    // 执行原函数 
     }
 }
+
+Function.prototype.after = function (afterfn) {
+    var __self = this;
+    return function () {
+        var ret = __self.apply(this, arguments);
+        afterfn.apply(this, arguments);
+        return ret;
+    }
+};
+
+var func = function () {
+    console.log(2);
+};
+
+func = func.before(function () {
+    console.log(1);
+}).after(function () {
+    console.log(3);
+});
+func();
+
+//单例模式 保证全局只有一个实例
+var getSingle = function (fn) {
+    var result;
+    return function () {
+        return result || (result = fn.apply(this, arguments));
+    }
+};
+
+//策略模式 优化大量if else  将操作封装成函数在调用
+
+var strategies = {
+    "S": function (salary) {
+        return salary * 4;
+    },
+    "A": function (salary) {
+        return salary * 3;
+    },
+    "B": function (salary) {
+        return salary * 2;
+    }
+};
+
+var calculateBonus = function (level, salary) {
+    return strategies[level](salary);
+};
+
+
+//代理模式
+
+var myImage = (function () {
+    var imgNode = document.createElement('img');
+    document.body.appendChild(imgNode);
+
+    return {
+        setSrc: function (src) {
+            imgNode.src = src;
+        }
+    }
+})();
+
+var proxyImage = (function () {
+    var img = new Image;
+    img.onload = function () {
+        myImage.setSrc(this.src);
+    }
+    return {
+        setSrc: function (src) {
+            myImage.setSrc('file:// /C:/Users/svenzeng/Desktop/loading.gif');
+            img.src = src;
+        }
+    }
+})();
+
+proxyImage.setSrc('http:// imgcache.qq.com/music/photo/k/000GGDys0yA0Nk.jpg'); 
